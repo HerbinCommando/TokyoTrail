58763ba0b9942faceb4930ba26993b20d5823b8d
Heath Gifford <heath@wgcells.com>
2017-07-29 22:57:11 -0700

    Move TextFormat into one file.

---
+++ src/com/gamejam/utils/TextFormats.hx
@@ -0,0 +1,17 @@
+package com.gamejam.utils;
+
+
+import openfl.text.TextFormat;
+
+
+class TextFormats {
+
+    public static var TITLES:TextFormat = new TextFormat("courier.ttf", 32, 0xBBBBBB);
+    public static var SUBTITLES:TextFormat = new TextFormat("courier.ttf", 24, 0xBBBBBB);
+    public static var BUTTONS:TextFormat = new TextFormat("courier.ttf", 24, 0xDEDEDE);
+    public static var FORM_PROPMPS:TextFormat = new TextFormat("courier.ttf", 26, 0x090909);
+    public static var LARGE_TEXT:TextFormat = new TextFormat("arial.ttf", 18, 0x090909);
+    public static var NORMAL_TEXT:TextFormat = new TextFormat("arial.ttf", 16, 0x090909);
+    public static var SMALL_TEXT:TextFormat = new TextFormat("arial.ttf", 12, 0x090909);
+
+}
